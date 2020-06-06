0625d6a94a4cd3b08363d40bbf150596a81cfff8
Melvin Cancino <melvin.cancino88@gmail.com>
2020-06-05 18:36:20 -0700

    Server app created

---
+++ app/src/test/java/com/project/servers/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.servers.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
