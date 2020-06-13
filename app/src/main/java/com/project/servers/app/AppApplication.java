cd7798d250e2ee63de9e4eb7435cd7a226e59377
Melvin Cancino <melvin.cancino88@gmail.com>
2020-06-13 16:34:50 -0700

    Final working version using Thymeleaf

--- app/src/main/java/com/project/servers/app/AppApplication.java
+++ app/src/main/java/com/project/servers/app/AppApplication.java
@@ -3,7 +3,7 @@ package com.project.servers.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+//This is the final working version using Thymeleaf
 @SpringBootApplication
 public class AppApplication {
 
