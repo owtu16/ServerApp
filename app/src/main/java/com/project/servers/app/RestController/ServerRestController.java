efef36a7ba5a8dad5a22bca657114d240b9676e2
Melvin Cancino <melvin.cancino88@gmail.com>
2020-06-16 11:34:59 -0700

    Made changes to the ServerRestController and and ServerServ

--- app/src/main/java/com/project/servers/app/RestController/ServerRestController.java
+++ app/src/main/java/com/project/servers/app/RestController/ServerRestController.java
@@ -3,6 +3,7 @@ package com.project.servers.app.RestController;
 import java.util.HashMap;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -18,108 +19,107 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project.servers.app.entity.Server;
-import com.project.servers.app.service.ServerService;
+import com.project.servers.app.service.ServerServ;
+
 
 //@CrossOrigin
 @RestController
 @RequestMapping("/servers")
-public class ServerRestController{
+public class ServerRestController {
 
-	private ServerService serverService;
-	
-	public ServerRestController(ServerService serverService) {
-		this.serverService = serverService;
+	@Autowired
+	private ServerServ serverServ;
+
+//	public ServerRestController(ServerService serverService) {
+//		this.serverService = serverService;
+//	}
+	@PostMapping("/save")
+	public String saveServer(@ModelAttribute("server") Server server) throws Exception {
+
+		serverServ.save(server);
+
+		return "redirect:/servers/list";
+
 	}
-	
-	@GetMapping("/showFormForAdd")
-	public String showFormForAdd(Model model) {
-		
-		Server server = new Server();
-		
-		model.addAttribute("server",server);
-		
-		return "/servers/server-form";
-	}
-	
-	@GetMapping("/showFormForUpdate")
-	public String showFormForUpdate(@RequestParam("serverId") int id, Model model) {
-		
-		Server server = serverService.findById(id);
-		
-		model.addAttribute("server",server);
-		
-		return "/servers/server-form";
-		
-	}
+
 	@PutMapping("/update")
-	public String updateServer(@ModelAttribute("server") Server server) {
-		serverService.update(server);
-		
+	public String updateServer(@ModelAttribute("server") Server server) throws Exception {
+		serverServ.update(server);
+
 		return "redirect:/servers/list";
 	}
-	
-	@PostMapping("/save") 
-	public String saveServer(@ModelAttribute("server") Server server) {
-		
-		serverService.save(server);
-		
-		return "redirect:/servers/list";
-		
-	}
-	// deletes server by passing the IP Address
+
 	@DeleteMapping("/delete/{ipAddress}")
-	public String delete(@PathVariable(value="ipAddress") String ip_address) {
-		
-	
-		serverService.delete(ip_address);
-		
-		
+	public String delete(@PathVariable(value = "ipAddress") String ip_address) throws Exception {
+
+		serverServ.delete(ip_address);
+
 		return "Server with id " + ip_address + " has been deleted";
 //		
 	}
-	// returns cluster of servers with location and # of servers at that particular location
-	@GetMapping("/cluster")
-	public List<Server> listByLocation(Model model) {
-		
-		List<Server> servers = serverService.findByLocation();
-		
-		model.addAttribute("servers",servers);
-		
-		return servers;
-		
-	}
+
 	// finds all servers regardless of location
 	@GetMapping("/findAll")
-	public List<Server> findAll(){
-		List<Server> servers = serverService.findAll();
+	public List<Server> findAll() throws Exception {
+		List<Server> servers = serverServ.findAll();
 		return servers;
 	}
-	// find servers depending on their location
-	@GetMapping("/showServersByLocation/{location}")
-	public List<Server> serversList(@PathVariable(value="location") String location, Model model) {
-		
-		// returns a list of server objects based on location
-		List<Server> servers = serverService.findAllByLocation(location);
-		model.addAttribute("servers",servers);
-//		return "/servers/servers-group";
+
+	// returns cluster of servers with location and # of servers at that particular
+	// location
+	@GetMapping("/cluster")
+	public HashMap<Server, Long> listByLocation(Model model) throws Exception {
+
+		HashMap<Server, Long> servers = serverServ.findByClusters();
+
+		model.addAttribute("servers", servers);
+
 		return servers;
+
 	}
 	
+	@GetMapping("/findByIp")
+	public Server findByIp(@PathVariable(value="ipAddress") String ipAddress, Model model) throws Exception {
+		
+		Server server = serverServ.findByIpAddress(ipAddress);
+		model.addAttribute("server",server);
+		
+		return server;
+		
+	}
+
+	// find servers depending on their location
+	@GetMapping("/showServersByLocation/{location}")
+	public List<Server> serversList(@PathVariable(value = "location") String location, Model model) throws Exception {
+
+		// returns a list of server objects based on location
+		List<Server> servers = serverServ.findAllByLocation(location);
+		model.addAttribute("servers", servers);
+//			return "/servers/servers-group";
+		return servers;
+	}
+
+//	@GetMapping("/showFormForAdd")
+//	public String showFormForAdd(Model model) {
+//
+//		Server server = new Server();
+//
+//		model.addAttribute("server", server);
+//
+//		return "/servers/server-form";
+//	}
+
+//	@GetMapping("/showFormForUpdate")
+//	public String showFormForUpdate(@RequestParam("serverId") int id, Model model) {
+//		
+//		Server server = serverServ.findById(id);
+//		
+//		model.addAttribute("server",server);
+//		
+//		return "/servers/server-form";
+//		
+//	}
+
+	// deletes server by passing the IP Address
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- app/src/main/java/com/project/servers/app/service/ServerServ.java
+++ app/src/main/java/com/project/servers/app/service/ServerServ.java
@@ -7,16 +7,20 @@ import com.project.servers.app.entity.Server;
 
 public interface ServerServ {
 
+	public String save(Server server) throws Exception;
+	
+	public String update(Server server) throws Exception;
+	
+	public String delete(String ipAddress) throws Exception;
+	
 	public List<Server> findAll() throws Exception;
+	
+	public HashMap<Server,Long> findByClusters() throws Exception;
+	
+	public Server findByIpAddress(String ipAddress) throws Exception;
 	
 	public List<Server> findAllByLocation(String ipAddress) throws Exception;
 	
-	public HashMap<Server,Long> findByClusters() throws Exception;
+	
 		
-	public String save() throws Exception;
-	
-	public String update() throws Exception;
-	
-	public String delete() throws Exception;
-	
 }
