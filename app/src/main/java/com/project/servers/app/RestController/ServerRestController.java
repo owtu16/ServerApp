be48b1aa9b4783f72b4224bed2ca2314924ef2d8
Melvin Cancino <melvin.cancino88@gmail.com>
2020-06-16 20:19:57 -0700

    Added some changes to fit in with my new DAO implementation

--- app/src/main/java/com/project/servers/app/RestController/ServerRestController.java
+++ app/src/main/java/com/project/servers/app/RestController/ServerRestController.java
@@ -3,6 +3,7 @@ package com.project.servers.app.RestController;
 import java.util.HashMap;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -23,103 +24,68 @@ import com.project.servers.app.service.ServerService;
 //@CrossOrigin
 @RestController
 @RequestMapping("/servers")
-public class ServerRestController{
+public class ServerRestController {
 
+	@Autowired
 	private ServerService serverService;
-	
-	public ServerRestController(ServerService serverService) {
-		this.serverService = serverService;
-	}
-	
-	@GetMapping("/showFormForAdd")
-	public String showFormForAdd(Model model) {
-		
-		Server server = new Server();
-		
-		model.addAttribute("server",server);
-		
-		return "/servers/server-form";
-	}
-	
-	@GetMapping("/showFormForUpdate")
-	public String showFormForUpdate(@RequestParam("serverId") int id, Model model) {
-		
-		Server server = serverService.findById(id);
-		
-		model.addAttribute("server",server);
-		
-		return "/servers/server-form";
-		
-	}
-	@PutMapping("/update")
-	public String updateServer(@ModelAttribute("server") Server server) {
-		serverService.update(server);
-		
-		return "redirect:/servers/list";
-	}
-	
-	@PostMapping("/save") 
-	public String saveServer(@ModelAttribute("server") Server server) {
-		
+
+	@PostMapping("/save")
+	public String saveServer(@ModelAttribute("server") Server server) throws Exception {
+
 		serverService.save(server);
-		
+
 		return "redirect:/servers/list";
-		
+
 	}
+
+	@PutMapping("/update")
+	public String updateServer(@ModelAttribute("server") Server server) throws Exception {
+		serverService.update(server);
+
+		return "redirect:/servers/list";
+	}
+
 	// deletes server by passing the IP Address
 	@DeleteMapping("/delete/{ipAddress}")
-	public String delete(@PathVariable(value="ipAddress") String ip_address) {
-		
-	
+	public String delete(@PathVariable(value = "ipAddress") String ip_address) throws Exception {
+
 		serverService.delete(ip_address);
-		
-		
+
 		return "Server with id " + ip_address + " has been deleted";
-//		
+
 	}
-	// returns cluster of servers with location and # of servers at that particular location
-	@GetMapping("/cluster")
-	public List<Server> listByLocation(Model model) {
-		
-		List<Server> servers = serverService.findByLocation();
-		
-		model.addAttribute("servers",servers);
-		
-		return servers;
-		
-	}
+
 	// finds all servers regardless of location
 	@GetMapping("/findAll")
-	public List<Server> findAll(){
+	public List<Server> findAll() throws Exception {
 		List<Server> servers = serverService.findAll();
 		return servers;
 	}
+
+
+
+	// returns cluster of servers with location and # of servers at that particular
+	// location
+	@GetMapping("/cluster")
+	public HashMap<String,Long> findCluster(Model model) throws Exception {
+
+		HashMap<String,Long> servers = serverService.findCluster();
+
+		model.addAttribute("servers", servers);
+
+		return servers;
+
+	}
+
 	// find servers depending on their location
 	@GetMapping("/showServersByLocation/{location}")
-	public List<Server> serversList(@PathVariable(value="location") String location, Model model) {
-		
+	public List<Server> serversList(@PathVariable(value = "location") String location, Model model) throws Exception {
+
 		// returns a list of server objects based on location
 		List<Server> servers = serverService.findAllByLocation(location);
-		model.addAttribute("servers",servers);
+		model.addAttribute("servers", servers);
 //		return "/servers/servers-group";
 		return servers;
 	}
-	
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
