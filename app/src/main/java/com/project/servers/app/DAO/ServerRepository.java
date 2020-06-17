b4454ed293a6e6c0d5c3d741eb6466a685cf7420
Melvin Cancino <melvin.cancino88@gmail.com>
2020-06-16 20:19:05 -0700

    I think I deleted this one

--- app/src/main/java/com/project/servers/app/DAO/ServerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.servers.app.DAO;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.project.servers.app.entity.Server;
-
-public interface ServerRepository extends JpaRepository<Server, String> {
-
-}
